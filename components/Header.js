4fa4a5ae2be8c974ad56b7ebbd69d3cbd0f2d0b0
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-12-01 17:35:05 -0300

    Header and footer

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <header>
+            <img src='/assets/projects/tgif/tgif.png' />
+            <nav>
+                <a href='#'>Home</a>
+                <a href='#about'>About me</a>
+                <a href='#projects'>Projects</a>
+                <a href='#contact'>Contact</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
