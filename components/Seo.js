ab7572cfb18a8850d96aac41d112efba6dcfcf78
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-06 21:33:26 -0300

    Locale files updated

--- components/Seo.js
+++ components/Seo.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 const Seo = ({ seo }) => {
-    const { title, description, ogTitle, ogDescription, ogUrl } = seo
+    const { title, description, ogUrl } = seo
 
     return (
         <Head>
@@ -9,7 +9,7 @@ const Seo = ({ seo }) => {
             <meta name="description" content={description} />
             <meta property='og:title' content={title} />
             <meta property='og:description' content={description} />
-            <meta property='og:image' />
+            <meta property='og:image' content='/assets/icon.jpg' />
             <meta property='og:url' content={ogUrl} />
             <meta property='og:type' content='website' />
             <link rel="icon" href="/favicon.ico" />
