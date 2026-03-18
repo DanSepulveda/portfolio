db533c1d9157587ed2b3183e7c19aa8022199081
Daniel Sepúlveda <dansepdev@gmail.com>
2026-03-17 21:07:44 -0300

    refactor: path alias updated

---
+++ src/consts/social.ts
@@ -0,0 +1,30 @@
+import type { SvgComponent } from "astro/types";
+
+import { SITE } from "./config";
+import Github from "@/assets/images/social/github.svg";
+import LinkedIn from "@/assets/images/social/linkedin.svg";
+
+export interface Social {
+  id: "github" | "linkedin";
+  name: "Github" | "LinkedIn";
+  url: string;
+  label: string;
+  image: SvgComponent;
+}
+
+export const SOCIALS: Social[] = [
+  {
+    id: "github",
+    name: "Github",
+    image: Github,
+    url: "https://github.com/DanSepulveda",
+    label: `Perfil de Github ${SITE.author}`,
+  },
+  {
+    id: "linkedin",
+    name: "LinkedIn",
+    image: LinkedIn,
+    url: "https://www.linkedin.com/in/dansepulvedap",
+    label: `Perfil de Linkedin ${SITE.author}`,
+  },
+];
